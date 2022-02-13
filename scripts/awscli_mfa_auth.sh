21458741eb121fa15a78df02905c8382763a39e6
William Vannuffelen <williamvannuffelen@gmail.com>
2022-02-13 10:27:59 +0100

    add script to init shell for mfa cli

---
+++ scripts/awscli_mfa_auth.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# requires jq
+
+# unset env vars first; otherwise get-session-token call fail due to AWS idiosyncracy
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_SESSION_TOKEN
+
+# vars
+# static var as I don't want to be queried for it every time
+MFAARN='arn:aws:iam::850901712561:mfa/wvannuffelen'
+
+read -p "Enter MFA code: " MFACODE
+
+# get and set secrets to shell session env vars
+res=$(aws sts get-session-token --serial-number $MFAARN --token-code $MFACODE --duration-seconds 7200)
+
+export AWS_ACCESS_KEY_ID=$(echo $res | jq -r '.Credentials.AccessKeyId')
+export AWS_SECRET_ACCESS_KEY=$(echo $res | jq -r '.Credentials.SecretAccessKey')
+export AWS_SESSION_TOKEN=$(echo $res | jq -r '.Credentials.SessionToken')
